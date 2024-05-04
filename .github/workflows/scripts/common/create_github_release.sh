36b91a8c2d82afe11758495c2f1fdb81449c0ef6
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2024-05-05 00:41:46 +0530

    Update create_github_release.sh

--- .github/workflows/scripts/common/create_github_release.sh
+++ .github/workflows/scripts/common/create_github_release.sh
@@ -48,7 +48,7 @@ echo "Tag pushed: ${RELEASE_TAG}"
 # Create the release using the GitHub API
 curl --request POST \
      --url https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
-     --header "Authorization: Bearer $GH_TOKEN" \
+     --header "Authorization: token $GH_TOKEN" \
      --header "X-GitHub-Api-Version: 2022-11-28" \
      --header "content-type: application/json" \
      --data "{
